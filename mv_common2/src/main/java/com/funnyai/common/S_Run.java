35e5d1a50c3083e0afd7412e1c9f469b30a54177
happyli <happyli.net@gmail.com>
2019-08-23 11:52:28 +0800

    Update S_Run.java

--- mv_common2/src/main/java/com/funnyai/common/S_Run.java
+++ mv_common2/src/main/java/com/funnyai/common/S_Run.java
@@ -45,8 +45,10 @@ public class S_Run {
             C_Run_Session_Min pSession=C_Run_Session_Min.Get_New_Session(0, 0);
             
             C_Command_Min pCommand=new C_Command_Min(pSession,0);
-            pCommand.RunShell("java -jar "+AI_Var2.Path_Root+"funny_sql.jar \""
-                    +strFile+"\" \""+strMax+"\" \""+SQL+"\" \""+strSep+"\" \""+Output+"\" \""+strUser+"\"");
+            String strLine="java -jar "+AI_Var2.Path_Root+"funny_sql.jar \""
+                    +strUser+"\" \""+strFile+"\" \""+strMax+"\" \""+SQL+"\" \""+strSep+"\" \""+Output+"\"";
+            out.println(strLine);
+            pCommand.RunShell(strLine);
             out.println("file_sql finished");
         } catch (IOException ex) {
             S_Debug.Write_DebugLog("error_sql", ex.toString());
